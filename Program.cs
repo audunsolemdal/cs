5c12c7586487e140b4a426b61c9a239638cd9f4f
solomson <audunlsolemdal@gmail.com>
2019-05-17 22:20:22 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Acme.Collections;
+
+namespace csproj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            Console.WriteLine("Hello World!");
+
+                    Stack s = new Stack();
+        s.Push(1);
+        s.Push(10);
+        s.Push(100);
+        Console.WriteLine(s.Pop());
+        Console.WriteLine(s.Pop());
+        Console.WriteLine(s.Pop());
+        }
+    }
+}
