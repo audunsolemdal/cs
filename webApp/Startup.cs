1a6ce329d365c74aaaa905d5753ae571647f8341
solomson <audunlsolemdal@gmail.com>
2019-05-18 20:44:18 +0200

    aspnet folder

---
+++ webApp/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace webApp
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+
+
+            // legg til tjenester (prepare)
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+        }
+            // gjør spesifikke ting for å sette opp miljøet ditt (dev/production)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+                // default oppstartsmelding
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello W2orld!");
+            });
+        }
+    }
+}
