ac52b22c59e58527c3e93bd0a4799cd2f4f1d497
solomson <audunlsolemdal@gmail.com>
2019-05-18 17:22:38 +0200

    test multiple projects in a folder

---
+++ consoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csproj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1;
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
